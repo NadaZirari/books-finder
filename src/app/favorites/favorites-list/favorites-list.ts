ea0ba8b28f235a5d36b1f85045c6257e4e5d7db6
NadaZirari <zirarinada20@gmail.com>
2026-04-16 12:54:49 +0100

    ajouter favorits list

--- src/app/favorites/favorites-list/favorites-list.ts
+++ src/app/favorites/favorites-list/favorites-list.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FavoriteService } from '../../core/services/favorite';
+import { FavoriteItem } from '../favorite-item/favorite-item';
+import { Loader } from '../../shared/loader/loader';
 
 @Component({
   selector: 'app-favorites-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, FavoriteItem, Loader],
   templateUrl: './favorites-list.html',
   styleUrl: './favorites-list.css',
 })
-export class FavoritesList {}
+export class FavoritesList {
+  public favoriteService = inject(FavoriteService);
+}
